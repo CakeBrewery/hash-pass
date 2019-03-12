1829b60bd08368857098c1adb7069ce9f45e4c63
Samuel N <1784174+CakeBrewery@users.noreply.github.com>
2019-03-12 11:07:43 -0700

    Fix whitespace

--- src/main.js
+++ src/main.js
@@ -2,25 +2,24 @@ var sgp = require('supergenpass-lib');
 
 
 function fillPasswords(password) {
-	function getPwdInputs() {
-		var result = [];
-		var inputs = document.getElementsByTagName("input");
+    function getPwdInputs() {
+        var result = [];
+        var inputs = document.getElementsByTagName("input");
 
-		for (var i = 0; i < inputs.length; i++) {
-			if (inputs[i].type.toLowerCase() === "password") {
-				result.push(inputs[i]);
-			}
-		}
-		return result;
-	}
+        for (var i = 0; i < inputs.length; i++) {
+            if (inputs[i].type.toLowerCase() === "password") {
+                result.push(inputs[i]);
+            }
+        }
+        return result;
+    }
 
-	var pwdInputs = getPwdInputs();
+    var pwdInputs = getPwdInputs();
 
-	for (var i = 0; i < pwdInputs.length; i++) {
-		pwdInputs[i].value = password;
-	}
+    for (var i = 0; i < pwdInputs.length; i++) {
+        pwdInputs[i].value = password;
+    }
 }
-
 
 
 function validateForm() {
@@ -37,98 +36,98 @@ function validateForm() {
 }
 
 
-document.addEventListener("DOMContentLoaded", function() {
-	chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-		var url = new URL(tabs[0].url);
-		document.getElementById('domain').value = url.hostname;
-	});
+document.addEventListener("DOMContentLoaded", function () {
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        var url = new URL(tabs[0].url);
+        document.getElementById('domain').value = url.hostname;
+    });
 
-	var btn = document.getElementById('generate-btn');
-	var $generated = $('#generated');
-	var $generatedContainer = $('#generated-container');
-	var $copy = $('#copy');
+    var btn = document.getElementById('generate-btn');
+    var $generated = $('#generated');
+    var $generatedContainer = $('#generated-container');
+    var $copy = $('#copy');
 
-	$generated.change(function() {
-		if ($generated.val() && $generated.val().length >= 1) {
-		    $generatedContainer.show();
-		} else {
-		    $generatedContainer.hide();
-		}
-	});
+    $generated.change(function () {
+        if ($generated.val() && $generated.val().length >= 1) {
+            $generatedContainer.show();
+        } else {
+            $generatedContainer.hide();
+        }
+    });
 
-	$copy.click(function() {
-		if ($generated.val() && $generated.val().length >= 1) {
-			$generated.select();
-			document.execCommand('copy');
-		}
-	});
+    $copy.click(function () {
+        if ($generated.val() && $generated.val().length >= 1) {
+            $generated.select();
+            document.execCommand('copy');
+        }
+    });
 
-	btn.addEventListener('click', function() {
-		if (!validateForm()) {
-		    $generated.val('') && $generatedContainer.hide();
-			return;
-		}
+    btn.addEventListener('click', function () {
+        if (!validateForm()) {
+            $generated.val('') && $generatedContainer.hide();
+            return;
+        }
 
-		// Load settings
+        // Load settings
         var includeSymbols = document.getElementById('symbols').checked;
         var length = parseInt($('#length').val());
 
         // Load required things
-		var password = document.getElementById('password').value;
-		var uri = document.getElementById('domain').value;
+        var password = document.getElementById('password').value;
+        var uri = document.getElementById('domain').value;
 
-		sgp.generate(password, uri, {length: length}, function(password) {
-			// Important.
-			if (includeSymbols) {
-				password += '!';
-			}
+        sgp.generate(password, uri, {length: length}, function (password) {
+            // Important.
+            if (includeSymbols) {
+                password += '!';
+            }
 
-			chrome.tabs.executeScript({
-				code: '(' + fillPasswords + ')(\"' + password + '\");'  // Seems oddly hacky.
-			});
+            chrome.tabs.executeScript({
+                code: '(' + fillPasswords + ')(\"' + password + '\");'  // Seems oddly hacky.
+            });
 
-			$generated.val(password) && $generated.change();
-			$generated.change();
-		});
-	});
+            $generated.val(password) && $generated.change();
+            $generated.change();
+        });
+    });
 
-	$('input').keypress(function(e) {
-		if (e.which === 13) {
-			$(btn).click();
-		}
-	});
+    $('input').keypress(function (e) {
+        if (e.which === 13) {
+            $(btn).click();
+        }
+    });
 
-	$('.stored-setting').change(function(){
-		var key = $(this).attr('id');
-		var value;
-	    if ($(this).attr('type') === 'checkbox') {
-			value = $(this).prop('checked');
-		} else {
-	    	value = $(this).val();
-		}
+    $('.stored-setting').change(function () {
+        var key = $(this).attr('id');
+        var value;
+        if ($(this).attr('type') === 'checkbox') {
+            value = $(this).prop('checked');
+        } else {
+            value = $(this).val();
+        }
 
-		var toStore = {};
-	    toStore[key] = value;
-		chrome.storage.sync.set(toStore);
-	}).each(function(i) {
-	    // Used to load all the settings when the extension is opened.
+        var toStore = {};
+        toStore[key] = value;
+        chrome.storage.sync.set(toStore);
+    }).each(function (i) {
+        // Used to load all the settings when the extension is opened.
         var $elem = $(this);
-		var key = $elem.attr('id');
+        var key = $elem.attr('id');
 
-		chrome.storage.sync.get(key, function(result){
-			var value = result[key];
+        chrome.storage.sync.get(key, function (result) {
+            var value = result[key];
 
             if (value) {
                 if ($elem.attr('type') === 'checkbox') {
                     $elem.prop('checked', value)
                 } else {
                     if ($elem.attr('type') === "number") {
-                    	value = parseInt(value);
-					}
+                        value = parseInt(value);
+                    }
                     $elem.val(value);
                 }
             }
-		});
-	});
+        });
+    });
 }, false);
 
